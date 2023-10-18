8a9a8b5728c96455059623fb124d8bd244ddcaa5
Zodomo <zodomo@proton.me>
2023-10-17 23:53:24 -0500

    updated interface

--- src/IAlignmentVault.sol
+++ src/IAlignmentVault.sol
@@ -9,13 +9,17 @@ interface IAlignmentVault {
     function vaultId() external view returns (uint256);
     function nftsHeld(uint256 _index) external view returns (uint256);
 
-    function initialize(address _erc721, address _owner, uint256 _vaultId) external;
-    function disableInitializers() external;
+    function initialize(address _erc721, address _owner, uint256 _vaultId) external payable;
+    function disableInitializers() external payable;
 
-    function alignMaxLiquidity() external;
-    function claimYield(address _recipient) external;
-    function checkInventory(uint256[] memory _tokenIds) external;
+    function alignNfts(uint256[] memory _tokenIds) external payable;
+    function alignTokens(uint256 _amount) external payable;
+    function alignMaxLiquidity() external payable;
+    function claimYield(address _recipient) external payable;
 
-    function rescueERC20(address _token, address _to) external returns (uint256);
-    function rescueERC721(address _token, address _to, uint256 _tokenId) external;
+    function checkInventory(uint256[] memory _tokenIds) external payable;
+    function getInventory() external view returns (uint256[] memory);
+
+    function rescueERC20(address _token, address _to) external payable returns (uint256);
+    function rescueERC721(address _token, address _to, uint256 _tokenId) external payable;
 }
