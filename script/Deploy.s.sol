8d3ecd1597e333f56dbb4fa805c23df2ad98f98a
Zodomo <zodomo@proton.me>
2024-03-29 11:05:22 -0500

    completely overhauled repo for NFTX V3 redesign/rewrite

---
+++ script/Deploy.s.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.23;
+
+import {Script, console2} from "../lib/forge-std/src/Script.sol";
+import {AlignmentVault} from "../src/AlignmentVault.sol";
+
+contract DeployScript is Script {
+
+}
