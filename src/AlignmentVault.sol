eb4bdbbc782476c05e9ff76d3293183945a3620a
Zodomo <zodomo@proton.me>
2024-03-29 13:20:10 -0500

    replaced NFTX lib with fork to resolve issues and implemented vault identification to initialize()

--- src/AlignmentVault.sol
+++ src/AlignmentVault.sol
@@ -5,6 +5,8 @@ import {Ownable} from "../lib/solady/src/auth/Ownable.sol";
 import {Initializable} from "../lib/openzeppelin-contracts/contracts/proxy/utils/Initializable.sol";
 import {IERC20} from "../lib/openzeppelin-contracts/contracts/interfaces/IERC20.sol";
 import {IERC721} from "../lib/openzeppelin-contracts/contracts/interfaces/IERC721.sol";
+import {INFTXVaultFactoryV3} from "../lib/nftx-protocol-v3/src/interfaces/INFTXVaultFactoryV3.sol";
+import {INFTXVaultV3} from "../lib/nftx-protocol-v3/src/interfaces/INFTXVaultV3.sol";
 
 /**
  * @title AlignmentVault
@@ -16,15 +18,48 @@ import {IERC721} from "../lib/openzeppelin-contracts/contracts/interfaces/IERC72
  * @custom:miyamaker https://miyamaker.com
  */
 contract AlignmentVault is Ownable, Initializable {
+    error AV_NFTX_NoVaultsExist();
+    error AV_NFTX_InvalidVaultId();
+    error AV_NFTX_InvalidVaultNFT();
+    error AV_NFTX_NoStandardVault();
+
+    uint256 private constant NFTX_STANDARD_FEE = 30000000000000000;
+
+    INFTXVaultFactoryV3 private constant NFTX_VAULT_FACTORY = INFTXVaultFactoryV3(0xC255335bc5aBd6928063F5788a5E420554858f01);
+
+    uint256 public vaultId;
     address public alignedNft;
 
     constructor() payable {}
 
     function initialize(
         address _owner,
-        address _alignedNft
-    ) external payable {
+        address _alignedNft,
+        uint256 _vaultId
+    ) external payable virtual initializer {
         _initializeOwner(_owner);
         alignedNft = _alignedNft;
+        if (_vaultId != 0) {
+            try NFTX_VAULT_FACTORY.vault(_vaultId) {
+                address vaultNft = NFTX_VAULT_FACTORY.vault(_vaultId);
+                if (vaultNft != _alignedNft) revert AV_NFTX_InvalidVaultNFT();
+                vaultId = _vaultId;
+            } catch {
+                revert AV_NFTX_InvalidVaultId();
+            }
+        } else {
+            address[] memory vaults = NFTX_VAULT_FACTORY.vaultsForAsset(_alignedNft);
+            if (vaults.length == 0) revert AV_NFTX_NoVaultsExist();
+            for (uint256 i; i < vaults.length; ++i) {
+                (uint256 mintFee, uint256 redeemFee, uint256 swapFee) = INFTXVaultV3(vaults[i]).vaultFees();
+                if (mintFee != NFTX_STANDARD_FEE || redeemFee != NFTX_STANDARD_FEE || swapFee != NFTX_STANDARD_FEE) continue;
+                else {
+                    _vaultId = INFTXVaultV3(vaults[i]).vaultId();
+                    vaultId = _vaultId;
+                    break;
+                }
+            }
+            if (_vaultId == 0) revert AV_NFTX_NoStandardVault();
+        }
     }
 }
