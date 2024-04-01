e41327f26607b70450ce9166bb59b510405bb0aa
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2024-04-02 01:09:17 +0700

    Update code comment

--- src/AlignmentVaultFactory.sol
+++ src/AlignmentVaultFactory.sol
@@ -1,9 +1,11 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.20;
 
+// Importing necessary libraries
 import "../lib/solady/src/utils/LibClone.sol";
 import "../lib/solady/src/auth/Ownable.sol";
 
+// Interface for Alignment Vault initialization
 interface IAVInitialize {
     function initialize(address _erc721, address _owner, uint256 _vaultId) external;
     function disableInitializers() external;
@@ -11,18 +13,21 @@ interface IAVInitialize {
 
 /**
  * @title AlignmentVaultFactory
- * @notice This can be used by any EOA or contract to deploy an AlignmentVault owned by the deployer.
- * @dev deploy() will perform a normal deployment. deployDeterministic() allows you to mine a deployment address.
+ * @notice This contract is used to deploy AlignmentVault contracts.
+ * @dev This contract allows for both normal and deterministic deployment of AlignmentVault contracts.
+ * @dev AlignmentVault contracts are used to store ERC721 tokens in a deterministic way.
  * @author Zodomo.eth (X: @0xZodomo, Telegram: @zodomo, GitHub: Zodomo, Email: zodomo@proton.me)
  */
 contract AlignmentVaultFactory is Ownable {
+    // Events
     event Deployed(address indexed deployer, address indexed vault);
     event Implementation(address indexed implementation);
 
-    address public implementation;
-    // Vault address => deployer address
-    mapping(address => address) public vaultDeployers;
+    // State variables
+    address public implementation; // Address of the implementation contract
+    mapping(address => address) public vaultDeployers; // Mapping to track deployer of each vault
 
+    // Constructor
     constructor(address _owner, address _implementation) payable {
         _initializeOwner(_owner);
         implementation = _implementation;
@@ -31,7 +36,7 @@ contract AlignmentVaultFactory is Ownable {
 
     /**
      * @notice Updates the implementation address used for new clones.
-     * @dev Does not affect previously deployed clones.
+     * @dev This function allows the contract owner to update the implementation contract.
      * @param _implementation The new implementation address for clones.
      */
     function updateImplementation(address _implementation) external virtual onlyOwner {
@@ -42,6 +47,7 @@ contract AlignmentVaultFactory is Ownable {
 
     /**
      * @notice Deploys a new AlignmentVault and fully initializes it.
+     * @dev This function deploys a new AlignmentVault contract and initializes it with the provided parameters.
      * @param _erc721 Address of the ERC721 token associated with the vault.
      * @param _vaultId NFTX Vault ID associated with _erc721
      * @return deployment Address of the newly deployed AlignmentVault.
@@ -56,6 +62,7 @@ contract AlignmentVaultFactory is Ownable {
 
     /**
      * @notice Deploys a new AlignmentVault to a deterministic address based on the provided salt.
+     * @dev This function allows deploying a new AlignmentVault to a deterministic address.
      * @param _erc721 Address of the ERC721 token associated with the vault.
      * @param _vaultId NFTX Vault ID associated with _erc721
      * @param _salt A unique salt to determine the address.
@@ -75,7 +82,7 @@ contract AlignmentVaultFactory is Ownable {
 
     /**
      * @notice Returns the initialization code hash of the clone of the implementation.
-     * @dev This is used primarily for tools like create2crunch to find vanity addresses.
+     * @dev This function returns the hash of the initialization code used in cloning.
      * @return codeHash The initialization code hash of the clone.
      */
     function initCodeHash() external view returns (bytes32 codeHash) {
@@ -84,6 +91,7 @@ contract AlignmentVaultFactory is Ownable {
 
     /**
      * @notice Predicts the address of the deterministic clone with the given salt.
+     * @dev This function predicts the address of a deterministic clone using the provided salt.
      * @param _salt The unique salt used to determine the address.
      * @return addr Address of the deterministic clone.
      */
