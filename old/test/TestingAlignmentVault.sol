70aff23b61298b4cb274ed4aae19825e8ffa33d2
Ade Febrian <59857124+Adefebrian@users.noreply.github.com>
2024-04-02 05:14:04 +0700

    Update code comments

--- old/test/TestingAlignmentVault.sol
+++ old/test/TestingAlignmentVault.sol
@@ -1,17 +1,19 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.20;
 
-import "openzeppelin/interfaces/IERC20.sol";
-import "openzeppelin/interfaces/IERC721.sol";
-import "../src/AlignmentVault.sol";
+import "openzeppelin/interfaces/IERC20.sol"; // Import ERC20 interface
+import "openzeppelin/interfaces/IERC721.sol"; // Import ERC721 interface
+import "../src/AlignmentVault.sol"; // Import AlignmentVault contract
 
 contract TestingAlignmentVault is AlignmentVault {
-    constructor() {}
-
+    constructor() {} // Constructor for TestingAlignmentVault contract
+    
+    // Function to call _estimateFloor() from AlignmentVault contract and return the result
     function call_estimateFloor() public view returns (uint256) {
         return _estimateFloor();
     }
 
+    // Function to view the address of the liquidity helper contract
     function view_liqHelper() public view returns (address) {
         return (address(_liqHelper));
     }
