1f1bd1185f9a8826babc4386a887efc34b6230d6
sudotx <64108848+sudotx@users.noreply.github.com>
2024-04-04 10:02:49 +0100

    beef up alignment vault test starter

--- test/AlignmentVault.t.sol
+++ test/AlignmentVault.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.23;
 
-import {Test, console2} from "../lib/forge-std/src/Test.sol";
+import "../lib/forge-std/src/Test.sol";
 import {AlignmentVault} from "../src/AlignmentVault.sol";
 import {IAlignmentVault} from "../src/IAlignmentVault.sol";
 import {FixedPointMathLib} from "../lib/solady/src/utils/FixedPointMathLib.sol";
@@ -14,17 +14,29 @@ import {IERC1155} from "../lib/openzeppelin-contracts-v5/contracts/interfaces/IE
 import {INFTXVaultFactoryV3} from "../lib/nftx-protocol-v3/src/interfaces/INFTXVaultFactoryV3.sol";
 import {INFTXVaultV3} from "../lib/nftx-protocol-v3/src/interfaces/INFTXVaultV3.sol";
 import {INFTXInventoryStakingV3} from "../lib/nftx-protocol-v3/src/interfaces/INFTXInventoryStakingV3.sol";
-import {INonfungiblePositionManager} from "../lib/nftx-protocol-v3/src/uniswap/v3-periphery/interfaces/INonfungiblePositionManager.sol";
+import {INonfungiblePositionManager} from
+    "../lib/nftx-protocol-v3/src/uniswap/v3-periphery/interfaces/INonfungiblePositionManager.sol";
 import {INFTXRouter} from "../lib/nftx-protocol-v3/src/interfaces/INFTXRouter.sol";
 import {ISwapRouter} from "../lib/nftx-protocol-v3/src/uniswap/v3-periphery/interfaces/ISwapRouter.sol";
 
 contract AlignmentVaultTest is Test {
     uint256 private constant NFTX_STANDARD_FEE = 30000000000000000;
     IWETH9 public constant WETH = IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    INFTXVaultFactoryV3 public constant NFTX_VAULT_FACTORY = INFTXVaultFactoryV3(0xC255335bc5aBd6928063F5788a5E420554858f01);
-    INFTXInventoryStakingV3 public constant NFTX_INVENTORY_STAKING = INFTXInventoryStakingV3(0x889f313e2a3FDC1c9a45bC6020A8a18749CD6152);
-    INonfungiblePositionManager public constant NPM = INonfungiblePositionManager(0x26387fcA3692FCac1C1e8E4E2B22A6CF0d4b71bF);
+    //@remind  is upgradable
+    //@remind pay attention to this integration
+    INFTXVaultFactoryV3 public constant NFTX_VAULT_FACTORY =
+        INFTXVaultFactoryV3(0xC255335bc5aBd6928063F5788a5E420554858f01);
+    //@remind  is upgradable
+    //@remind Allows users to stake vTokens to earn fees in vTokens and WETH. The position is minted as xNFT.
+    INFTXInventoryStakingV3 public constant NFTX_INVENTORY_STAKING =
+        INFTXInventoryStakingV3(0x889f313e2a3FDC1c9a45bC6020A8a18749CD6152);
+
+    //@remind Wraps Uniswap V3 positions in the ERC721 non-fungible token interface
+    INonfungiblePositionManager public constant NPM =
+        INonfungiblePositionManager(0x26387fcA3692FCac1C1e8E4E2B22A6CF0d4b71bF);
+    //@remind  Router to facilitate vault tokens minting/burning + addition/removal of concentrated liquidity
     INFTXRouter public constant NFTX_POSITION_ROUTER = INFTXRouter(0x70A741A12262d4b5Ff45C0179c783a380EebE42a);
+    //@remind Router for stateless execution of swaps against Uniswap V3
     ISwapRouter public constant NFTX_SWAP_ROUTER = ISwapRouter(0x1703f8111B0E7A10e1d14f9073F53680d64277A3);
 
     AlignmentVault public av;
@@ -40,15 +52,37 @@ contract AlignmentVaultTest is Test {
     address public constant MILADY = 0x5Af0D9827E0c53E4799BB226655A1de152A425a5;
     uint256 public constant VAULT_ID = 5;
 
+    address deployer;
+    address attacker;
+
     function setUp() public virtual {
+        vm.createSelectFork("mainnet");
         av = new AlignmentVault();
-        av.initialize(address(this), MILADY, VAULT_ID);
-        av.disableInitializers();
+        vm.label(address(av), "AlignmentVault");
+        vm.label(address(MILADY), "Milady NFT");
+        vm.label(address(NFTX_VAULT_FACTORY), "NFTX Vault Factory");
+        vm.label(address(NFTX_SWAP_ROUTER), "NFTX Swap Router");
+        vm.label(address(NFTX_POSITION_ROUTER), "NFTX Position Router");
+        vm.label(address(NFTX_INVENTORY_STAKING), "NFTX Inventory Staking");
+        deployer = makeAddr("deployer");
+        attacker = makeAddr("attacker");
+
         vm.deal(address(av), 10 ether);
+
+        vm.startPrank(deployer);
+        av.initialize(address(deployer), MILADY, VAULT_ID);
+        av.disableInitializers();
         vault = av.vault();
         vaultId = VAULT_ID;
         alignedNft = MILADY;
         setApprovals();
+        vm.stopPrank();
+    }
+
+    modifier prank(address who) {
+        vm.startPrank(who);
+        _;
+        vm.stopPrank();
     }
 
     receive() external payable {}
@@ -58,7 +92,8 @@ contract AlignmentVaultTest is Test {
     }
 
     function targetInitialize(address alignedNft_, uint256 vaultId_) public {
-        av = new AlignmentVault();
+        // av = new AlignmentVault();
+        console2.log("alignment vault: ", address(av));
         vault = NFTX_VAULT_FACTORY.vault(vaultId_);
         vaultId = vaultId_;
         alignedNft = alignedNft_;
@@ -79,9 +114,11 @@ contract AlignmentVaultTest is Test {
 
         for (uint256 i; i < vaults.length; ++i) {
             (uint256 mintFee, uint256 redeemFee, uint256 swapFee) = INFTXVaultV3(vaults[i]).vaultFees();
-            if (mintFee != NFTX_STANDARD_FEE || redeemFee != NFTX_STANDARD_FEE || swapFee != NFTX_STANDARD_FEE) continue;
-            else if (INFTXVaultV3(vaults[i]).manager() != address(0)) continue;
-            else {
+            if (mintFee != NFTX_STANDARD_FEE || redeemFee != NFTX_STANDARD_FEE || swapFee != NFTX_STANDARD_FEE) {
+                continue;
+            } else if (INFTXVaultV3(vaults[i]).manager() != address(0)) {
+                continue;
+            } else {
                 vaultId = INFTXVaultV3(vaults[i]).vaultId();
                 vault = vaults[i];
                 break;
@@ -109,7 +146,8 @@ contract AlignmentVaultTest is Test {
                 tokenCount += amounts[i];
             }
         }
-        uint256 ethRequired = FixedPointMathLib.fullMulDivUp(INFTXVaultV3(vault).vTokenToETH(tokenCount * 1 ether), 30000, 1000000);
+        uint256 ethRequired =
+            FixedPointMathLib.fullMulDivUp(INFTXVaultV3(vault).vTokenToETH(tokenCount * 1 ether), 30000, 1000000);
         INFTXVaultV3(vault).mint{value: ethRequired}(tokenIds, amounts, address(this), address(this));
     }
 
